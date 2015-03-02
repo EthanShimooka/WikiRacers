a1202df8a9c812a48b99d51ed9d39f5b19ff1805
EthanShimooka <sshimook@ucsc.edu>
2015-03-01 19:10:52 -0800

    Merge branch 'Viet1' into Master
    Changes to the Winner page

--- app/src/main/java/wikiracers/wikiracers/WinnerPage.java
+++ app/src/main/java/wikiracers/wikiracers/WinnerPage.java
@@ -1,71 +1,74 @@
 package wikiracers.wikiracers;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
 import android.widget.Button;
-import android.text.TextUtils;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-/*
-*This is the page after you've won the game, it will redirect you to this page.
-*Tried to do an alert dialog instead of redirecting to another page but have some issues atm
-*/
-//Todo: get name, get counts test
-
-public class WinnerPage extends Activity {
-
-
-
+import java.net.URL;
+import android.util.Log;
+// Alert Dialog Pops up after winning pop
+public class WinnerPage extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.winner_page);
-        statisticbtn();
         menuButton();
 
-    }
+        LayoutInflater inflater= LayoutInflater.from(this);
+        View view=inflater.inflate(R.layout.winner_page, null);
+        TextView textview=(TextView)view.findViewById(R.id.scroll);
 
-    //Redirects to stats page
-    private void statisticbtn(){
-        Button statsbtn = (Button) findViewById(R.id.statisticsbutton);
-        View.OnClickListener statsYell = new View.OnClickListener() {
+
+
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("You're a Winner!");
+        alertDialog.setMessage("This is your path count: " + WebBrowser.pageCount + "\n" + "\n" + "This is your Path: "+ "\n"  + WebBrowser.list_URL);
+        alertDialog.setIcon(R.drawable.ic_launcher);
+
+        alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "Share It", new DialogInterface.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), SettingsPage.class));
-                Util.playWavSound(getApplicationContext(), "select");
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Confirm", Toast.LENGTH_SHORT).show();
 
             }
-        };
-        statsbtn.setOnClickListener(statsYell);
+        });
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Button", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Affirmative", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Main Menu", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                Toast.makeText(getApplicationContext(), "Ay", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        alertDialog.show();
     }
     //Redirects to menuActivity
-    private void menuButton(){
+    private void menuButton() {
         Button menuBtn = (Button) findViewById(R.id.menu_button);
         View.OnClickListener statsYell = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), MenuActivity.class));
-                Util.playWavSound(getApplicationContext(), "select");
+            }
+        };
+        menuBtn.setOnClickListener(statsYell);
+    }
 
-            }
-        };
-        menuBtn.setOnClickListener(statsYell);
-    }
-    private void hibtn(){
-        Button menuBtn = (Button) findViewById(R.id.hiscorebutton);
-        View.OnClickListener statsYell = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(getApplicationContext(), HiscorePage.class));
-            }
-        };
-        menuBtn.setOnClickListener(statsYell);
-    }
+
 }
