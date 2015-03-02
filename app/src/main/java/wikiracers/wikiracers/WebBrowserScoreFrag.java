09b049285e710aef4167b25a9d44811b4f5f159d
EthanShimooka <sshimook@ucsc.edu>
2015-03-01 18:20:14 -0800

    Added Score Button Fragment

---
+++ app/src/main/java/wikiracers/wikiracers/WebBrowserScoreFrag.java
@@ -0,0 +1,31 @@
+package wikiracers.wikiracers;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * =========================================
+ * Authored by Ethan , 3/1/2015
+ * Project : WikiRacers
+ * =========================================
+ */
+public class WebBrowserScoreFrag  extends Fragment{
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            return inflater.inflate(R.layout.fragment_web_browser_score, container, false);
+
+
+        }
+
+
+
+
+}
+
